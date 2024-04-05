e42702be612fadc1196e519839cd987017ef04a0
antonioortegas <antonioortega@uma.es>
2024-04-05 13:44:53 +0200

    generating docstrings example

--- tests.py
+++ tests.py
@@ -1,2 +1,14 @@
+def suma(a, b):
+    """returns the sum of two numbers
+
+    Args:
+        a (int): first number
+        b (int): second number
+
+    Returns:
+        int: sum of a and b
+    """
+    return a + b
+
 a = 5
-print(a^66)
+print(suma(a, 3))
