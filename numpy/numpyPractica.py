bba553b32660975ace185c3fe0a774422bfe6bdb
antonioortegas <antonioortega@uma.es>
2024-05-04 23:14:24 +0200

    assignment 1

--- numpy/numpyPractica.py
+++ numpy/numpyPractica.py
@@ -24,7 +24,7 @@ print(np.mean(array, axis=0))
 # 6
 array = np.arange(12)
 print(array)
-print(array.reshape(4, 3))
+print(array.reshape(4, 3)) 
 
 # 7
 array = np.arange(12)
@@ -52,23 +52,44 @@ array = np.random.randint(0, 10, 20)
 print(array)
 print(np.unique(array, return_counts=True))
 
-# 10
-
-
-# 11
-
+# 10 / 11
+#Normaliza un array bidimensional de 4 filas y 3 columnas restando la media y dividiendo por la desviación típica en cada fila.
+array = np.random.randint(0, 10, 12).reshape(4, 3)
+print(array)
+mean = np.mean(array, axis=1, keepdims=True)
+std = np.std(array, axis=1, keepdims=True)
+print((array - mean) / std)
 
 # 12
-
+array = np.random.randint(0, 10, 12).reshape(4, 3)
+print(array)
+print(np.unravel_index(np.argmax(array), array.shape))
+print(np.unravel_index(np.argmin(array), array.shape))
 
 # 13
-
+array = np.random.randint(0, 10, 12).reshape(4, 3)
+print(array)
+orden = np.argsort(array[:, 0])
+print(orden)
+print(array[orden])
 
 # 14
-
+array = np.random.normal(size=35).reshape(7, 5)
+array[array < 0] = 0
+print(array)
 
 # 15
-
+array = np.random.randint(0, 100, 32)
+print(array)
+k = np.random.randint(0, 100)
+print(k)
+# get all the indexes of the elements that are greater than k
+indexes = np.where(array > k)
+print(indexes)
 
 # 16
-
+array = np.random.uniform(0, 1, 42).reshape(6, 7)
+print(array)
+array[:, :2] = 0
+array[:, -3:] = 1
+print(array)
