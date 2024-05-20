24c446ee2105c9def76d1daf475fbbffbbb09ca2
antonioortegas <antonioortega@uma.es>
2024-05-20 22:53:07 +0200

    preprocessing script to format raw input data file into a standard csv

---
+++ scikit/preprocessing.py
@@ -0,0 +1,33 @@
+#load the raw .data from the file to give it the correct format
+import csv
+
+# load the data from the file
+with open("./scikit/data/flare.data2", "r") as f:
+    data = f.read()
+
+# split the data into rows
+data = data.split('\n')
+data = data[1:-1] # remove header because it is not part of the data and the last empty line
+# 
+columns = ["",
+           "modifed Zurich class",
+           "largest spot size",
+           "spot distribution",
+           "activity",
+           "evolution",
+           "previous 24 hr flare activity code",
+           "historically-complex",
+           "did region become historically complex",
+           "area",
+           "largest spot area",
+           "C-class flares",
+           "M-class flares",
+           "X-class flares",]
+
+with open("./scikit/test.csv", "w", newline="") as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(columns)
+    for index, row in enumerate(data):
+        row = [index] + row.split(' ')
+        csv_writer.writerow(row)
+    
--- scikit/tests.py
+++ scikit/tests.py
@@ -1,7 +1,20 @@
-import ucimlrepo
-from ucimlrepo import fetch_ucirepo
+#pip install ucimlrepo
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+solar_flare = fetch_ucirepo(id=89) 
+  
+# data (as pandas dataframes) 
+X = solar_flare.data.features 
+y = solar_flare.data.targets 
+  
+# metadata 
+print(solar_flare.metadata) 
+  
+# variable information 
+print(solar_flare.variables)
 
-df = fetch_ucirepo(id=89)
-print(df.data.features)
-print(df.data.targets)
-df.data.features.to_csv('data.csv')
+csv = solar_flare.data.original.to_csv()
+print(csv)
+#save to a file
+solar_flare.data.original.to_csv('./scikit/importcsv.csv')
