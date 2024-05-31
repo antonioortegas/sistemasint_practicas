eb96e4140bda45b3d2beb575640673bc513c1455
antonioortegas <antonioortega@uma.es>
2024-05-31 05:35:58 +0200

    script para la entrega casi listo, falta ROC

--- scikit/scripts.py
+++ scikit/scripts.py
@@ -13,36 +13,35 @@ from sklearn.model_selection import cross_validate
 import numpy as np
 
 pathToCsv = "./scikit/data/data.csv"
-kf = KFold(n_splits=10, shuffle=True)
 
 # load the dataframe from the csv file
-# the csv has ; as separator instead of , for some dumb reason
 df = pd.read_csv(pathToCsv, sep=';')
+
+kf = KFold(n_splits=10, shuffle=True)
+
 # in the last column, substitute all values "Enrolled" with "Graduate"
 # this is so that the target column has only 2 values. Since the dataset does not have any discernible way to determine if a student will continue or not
 # I will consider only 2 possibilities: the student will drop out, or not (so binary classification)
 # this seems to have had the biggest impact on the accuracy, jumping from ~0.73 to ~0.85 in both models
 df["Target"] = df["Target"].replace("Enrolled", "Graduate")
-print(df.head())
-# print number of dropouts, and number of graduates
-print("Number of dropouts: ", len(df[df["Target"] == "Dropout"]))
-print("Number of graduates: ", len(df[df["Target"] == "Graduate"]))
 
-# convert the target column to a numerical value, 0 for Dropout, 1 for Enrolled, 2 for Graduate
+# convert the target column to a numerical value
 le = LabelEncoder()
 df["Target"] = le.fit_transform(df["Target"])
-#print(df.head())
 
+correlation = df.corr()["Target"]
+# compute the absolute value of the correlation and sort by it
+correlation = correlation.abs().sort_values(ascending=False)
+print(correlation)
+
+# Separate the target column from the rest of the data
+y = df["Target"] # only the target column
 X = df.drop(columns=["Target"]) # drop the target column
-y = df["Target"]
-# drop all the columns that dont seem to have any impact on the accuracy (tested by removing them one by one and checking the accuracy)
-X = X.drop(X.columns[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,]], axis=1)
+
+# drop all the columns with correlation less than 0.1
+columns_to_drop = correlation[correlation < 0.1].index
+X = X.drop(columns=columns_to_drop)
 print(X.columns)
-# column 16 (tuition fees up to date) seems to have a big impact on the accuracy (about 0.05). Seems plausible. If the student can't pay the fees, they will have to drop out
-# columns 29 to 32 (related to the performance of the student on the 2nd semester) seem to have a 0.2-0.3 impact on the accuracy
-# my guess is that if the student is doing well on the 2nd semester, they are more likely to continue. Also, they most likely did well on the 1st semester too, so the 1st semester columns are not needed
-# columns 33 to 35 (related to the economic situation [inflation, unemployment, ans GDP]) seem to have a 0.2-0.3 impact on the accuracy
-# the accuracy seems to stabilize at around 0.75 before removing any columns, and removing any of the columns mentioned above seems to keep it there
 
 # Standardize the features
 scaler = StandardScaler() # after testing, the type of scaler doesn't seem to matter much, so I'll use the default one
@@ -60,65 +59,62 @@ decision_tree = DecisionTreeClassifier()
 
 # define some values for each hyperparameter as a starting point
 param_grid = {
-    'criterion': ['gini'], # doesn't seem to matter much, so I'll fix it to gini
-    'max_depth': [4], # 10 is already overfitting, so i will try around 5. 3 to 4 seems to be the best
-    'min_samples_split': [2], # doesn't seem to matter much, so I'll fix it to 2 which is the default
-    'min_samples_leaf': [1], # doesn't seem to matter much, so I'll fix it to 1 which is the default
-    # overall this seems to improve the accuracy from 0.79 to 0.85
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [4, 2, 6, 8],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 5, 8],
 }
 
 # validate with 10K-fold cross validation
 grid_search = GridSearchCV(estimator=decision_tree, param_grid=param_grid, cv=kf, scoring='accuracy', n_jobs=-1, verbose=1)
 grid_search.fit(X_train, y_train)
 best_decision_tree = grid_search.best_estimator_
-print(best_decision_tree.score(X_test, y_test))
+print("DT: ",best_decision_tree.score(X_test, y_test))
 # show each set of hyperparameters and the corresponding accuracy formatted as a string
-results = pd.DataFrame(grid_search.cv_results_)
-results = results[['params', 'mean_test_score']]
-print(results.to_string())
+# results = pd.DataFrame(grid_search.cv_results_)
+# results = results[['params', 'mean_test_score']]
+# print(results.to_string())
 
 # train a neural network model and calculate the accuracy
 multilayer_perceptron = MLPClassifier()
 
 # define some values for each hyperparameter as a starting point
 param_grid = {
-    'hidden_layer_sizes': [(10,)], # doesn't seem matter too much
-    'solver': ['adam'], # doesn't seem to matter much
+    'hidden_layer_sizes': [(10,)],
+    'solver': ['adam'],
     'max_iter': [500],
-    'alpha': [0.2], # between 0.1 and 0.5 seems to be ok
+    'alpha': [0.2],
     'learning_rate_init': [0.01],
-    'activation': ['relu'], # doesn't seem to matter much
+    'activation': ['relu'],
 }
-
+# validate with 10K-fold cross validation
 grid_search = GridSearchCV(estimator=multilayer_perceptron, param_grid=param_grid, cv=kf, scoring='accuracy', n_jobs=-1, verbose=1)
 grid_search.fit(X_train, y_train)
 best_neural_network = grid_search.best_estimator_
-print(best_neural_network.score(X_test, y_test))
+print("MLP: ", best_neural_network.score(X_test, y_test))
 # show each set of hyperparameters and the corresponding accuracy formatted as a string
-results = pd.DataFrame(grid_search.cv_results_)
-results = results[['params', 'mean_test_score']]
-print(results.to_string())
+# results = pd.DataFrame(grid_search.cv_results_)
+# results = results[['params', 'mean_test_score']]
+# print(results.to_string())
 
-# use cross_validate to get some metrics
+# use cross_validate to get some more metrics
 # metrics to calculate
-scoring = ['accuracy', 'f1', 'precision', 'recall']
+scoring = ['f1', 'precision', 'recall']
 
 # Evaluate Decision Tree
 decision_tree_scores = cross_validate(best_decision_tree, X_test, y_test, cv=kf, scoring=scoring)
-print(decision_tree_scores)
+# print(decision_tree_scores)
 print("Decision Tree")
 for metric in scoring:
     print(f"{metric}: {decision_tree_scores['test_' + metric].mean()}")
 
 # Evaluate Neural Network
-mlp_scores = cross_validate(best_neural_network, X_test, y_test, cv=kf, scoring=scoring)
-print("\nNeural Network")
+multilayer_perceptron_scores = cross_validate(best_neural_network, X_test, y_test, cv=kf, scoring=scoring)
+print("Neural Network")
 # cross_validate returns a dictionary with the scores for each metric in an array
 # to access each score, we need to access the key 'test_' + name_of_the_metric
 # then we can calculate the mean for that metric
 for metric in scoring:
-    print(f"{metric}: {mlp_scores['test_' + metric].mean()}")
-# TODO see if this metrics are enough
+    print(f"{metric}: {multilayer_perceptron_scores['test_' + metric].mean()}")
     
-# TODO plotting the ROC curve for both best models
-# TODO dunno if I should use cv for the curves or not
+# TODO ROC curves
